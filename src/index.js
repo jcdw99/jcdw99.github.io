361bfb66db852e762156d8a073f1558f405d728a
Justin De Witt <jcdw99@gmail.com>
2022-11-19 13:54:21 +0200

    removed not working google analytics stuff

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ReactGA from 'react-ga4'
-
-ReactGA.initialize('G-L0BHCMH3MW')
 
 ReactDOM.render(
   <React.StrictMode>
