d6399393eef9858769f403f5681644d564a5dbdd
Justin DeWitt <justindewitt@Justins-MacBook-Pro.local>
2022-11-19 13:28:26 +0200

    attempting google analytics integration, it cant be this easy?

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-L0BHCMH3MW')
 
 ReactDOM.render(
   <React.StrictMode>
