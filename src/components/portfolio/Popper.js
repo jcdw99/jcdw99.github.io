72220f05a555fced792657f255af23e08a51e7f7
Justin De Witt <jcdw99@gmail.com>
2022-11-29 12:47:35 +0200

    changed popper to popover, lets see if this is better?

--- src/components/portfolio/Popper.js
+++ src/components/portfolio/Popper.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Popper from '@mui/material/Popper';
-import PopupState, { bindToggle, bindPopper } from 'material-ui-popup-state';
-import Fade from '@mui/material/Fade';
-import Paper from '@mui/material/Paper';
+import Popover from '@mui/material/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
+
 import Box from "@mui/material/Box";
 
 export default function PopperPopupState(props) {
@@ -14,18 +13,22 @@ export default function PopperPopupState(props) {
         <PopupState variant="popper" popupId="demo-popup-popper">
         {(popupState) => (
             <Box textAlign='center'>
-            <Button variant='outlined' {...bindToggle(popupState)} style={{color:'#ffffff'}}>
+            <Button variant='outlined' {...bindTrigger(popupState)} style={{color:'#ffffff'}}>
                 {text}
             </Button>
-            <Popper {...bindPopper(popupState)} transition>
-                {({ TransitionProps }) => (
-                <Fade {...TransitionProps} timeout={350}>
-                    <Paper style={{padding: "0 10% 0 10%"}}>
-                        <Typography variant='p' style={{fontSize:'.6rem'}}>{desc}</Typography>
-                    </Paper>
-                </Fade>
-                )}
-            </Popper>
+            <Popover
+            {...bindPopover(popupState)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            >
+            <Typography variant='p' style={{fontSize:'.7rem'}}>{desc}</Typography>
+          </Popover>
             </Box>
         )}
         </PopupState>
