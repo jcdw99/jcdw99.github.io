e2435260eefc34603d83783b89cb1b32e9f86fb3
Justin De Witt <jcdw99@gmail.com>
2022-11-29 13:30:48 +0200

    removed width height console log

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -7,7 +7,6 @@ import useWindowDimensions from "../WindowDim"
 function PortfolioBlock(props) {
    const {name, desc, github, image} = props;
    const { height, width } = useWindowDimensions();   
-   console.log(height, width)
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box justifyContent={'center'} alignItems={'center'} display={'flex'} gap={'1.5rem'} fontSize={{xs: '2rem', md: '2.5rem'}}>  
