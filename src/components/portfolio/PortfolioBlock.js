3cc3f06598409b70834a79a60f421d07e242dbd2
Justin De Witt <jcdw99@gmail.com>
2022-11-29 13:19:36 +0200

    made portfolio page slightly more responsive

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -10,18 +10,18 @@ function PortfolioBlock(props) {
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box justifyContent={'center'} alignItems={'center'} display={'flex'} gap={'1.5rem'} fontSize={{xs: '2rem', md: '2.5rem'}}>  
-            <Typography sx={{ typography: { sm: 'h5', xs: 'h6' } }}> {name} </Typography> 
+            <Typography sx={{ typography: { sm: 'h4', xs: 'h6' }, fontWeight:'bold'}} > {name} </Typography> 
 
                <SocialIcon key={name} link={github} icon={"fa fa-github"} label={""} visible={github}/>
             
          </Box>
          <Box component={'img'} src={image} alt={'mockup'}  height={{xs: '90%', s:'80%', md: '70%'}} width={{xs: '90%', s:'80%', md: '70%'}} padding={"0 0 0 0"}/>
 
-         <Grid container  height={{xs: '50%', md: '40%'}} width={{xs: '80%', md: '70%'}}>
+         <Grid container  height={{xs: '50%', s: '50%', md: '20rem'}} width={{xs: '80%', s: '80%', md: '70%'}}>
             {/* This should be the description box */}
             <Grid item xs={12}  s={12} md={12} marginTop={'1vh'}>
 
-                 { Math.min(height, width) > 500 ?
+                 { Math.min(height, width) > 600 ?
                      <Paper style={{backgroundColor:'#1f1f1f'}} variant='outline' elevation={10}>  
                         <Typography variant='p' style={{fontSize:'.7rem'}}> {desc} </Typography> 
                      </Paper>  
